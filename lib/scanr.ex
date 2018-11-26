9077be724749deb0bfd4454aaa1d86a9088a2dcc
Emilio Nyaray <emilio@nyaray.com>
2018-11-27 00:06:57 +0100

    Initial commit

---
+++ lib/scanr.ex
@@ -0,0 +1,18 @@
+defmodule Scanr do
+  @moduledoc """
+  Documentation for Scanr.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Scanr.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/scanr_test.exs
@@ -0,0 +1,8 @@
+defmodule ScanrTest do
+  use ExUnit.Case
+  doctest Scanr
+
+  test "greets the world" do
+    assert Scanr.hello() == :world
+  end
+end
